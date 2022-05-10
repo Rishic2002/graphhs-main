9087c4e56e43282b322718bd6e1950f6c429fb9d
Rishic2002 <73890141+Rishic2002@users.noreply.github.com>
2022-05-10 17:44:15 +0530

    Update atthomepage.dart

--- lib/atthomepage.dart
+++ lib/atthomepage.dart
@@ -3,6 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:cupertino_icons/cupertino_icons.dart';
 
 class ATTHomePage extends StatefulWidget {
   @override
@@ -69,50 +70,182 @@ class _ATTHomePageState extends State<ATTHomePage> {
     );
   }
 
+  @override
   Widget _buildChart(BuildContext context, List<Att> Attdata) {
     mydata = Attdata;
     _generateData(mydata);
-    return Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Container(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text(
-                'att BY MONTH',
-                style: TextStyle(fontSize: 24.0, fontFamily: 'RobotoMono'),
-              ),
-              SizedBox(
-                  width: 900,
-                  height: 400,
-                  child: Card(
-                    child: Expanded(
-                      child: charts.LineChart(
-                        _seriesLineData,
-                        animate: true,
-                        defaultRenderer:
-                            new charts.LineRendererConfig(includePoints: true),
-                        //POINTS
-                        animationDuration:
-                            Duration(seconds: 5), //DURATION OF ANIMATION
-                        behaviors: [
-                          new charts.SeriesLegend(
-                            position: charts.BehaviorPosition.end,
-                            outsideJustification:
-                                charts.OutsideJustification.endDrawArea,
-                            horizontalFirst: false,
-                            desiredMaxRows: 2,
-                            cellPadding:
-                                new EdgeInsets.only(right: 4.0, bottom: 4.0),
-                            entryTextStyle: charts.TextStyleSpec(
-                                color: charts.Color(r: 127, g: 63, b: 191),
-                                fontFamily: 'Georgia',
-                                fontSize: 11),
-                          )
-                        ],
-                      ),
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xff1976d2),
+            //backgroundColor: Color(0xff308e1c),
+            bottom: TabBar(
+              indicatorColor: Color(0xff9962D0),
+              tabs: [
+                Tab(
+                  icon: new Icon(Icons.bookmark),
+                ),
+                Tab(
+                  icon: new Icon(Icons.bookmark),
+                ),
+                Tab(
+                  icon: new Icon(Icons.bookmark),
+                ),
+              ],
+            ),
+            title: Text('Flutter Charts'),
+          ),
+          body: TabBarView(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          'att BY MONTH',
+                          style: TextStyle(
+                              fontSize: 24.0, fontFamily: 'RobotoMono'),
+                        ),
+                        SizedBox(
+                            width: 900,
+                            height: 400,
+                            child: Card(
+                              child: Expanded(
+                                child: charts.LineChart(
+                                  _seriesLineData,
+                                  animate: true,
+                                  defaultRenderer:
+                                      new charts.LineRendererConfig(
+                                          includePoints: true),
+                                  //POINTS
+                                  animationDuration: Duration(
+                                      seconds: 5), //DURATION OF ANIMATION
+                                  behaviors: [
+                                    new charts.SeriesLegend(
+                                      position: charts.BehaviorPosition.end,
+                                      outsideJustification: charts
+                                          .OutsideJustification.endDrawArea,
+                                      horizontalFirst: false,
+                                      desiredMaxRows: 2,
+                                      cellPadding: new EdgeInsets.only(
+                                          right: 4.0, bottom: 4.0),
+                                      entryTextStyle: charts.TextStyleSpec(
+                                          color: charts.Color(
+                                              r: 127, g: 63, b: 191),
+                                          fontFamily: 'Georgia',
+                                          fontSize: 11),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ))
+                      ],
                     ),
-                  ))
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          'att BY MONTH',
+                          style: TextStyle(
+                              fontSize: 24.0, fontFamily: 'RobotoMono'),
+                        ),
+                        SizedBox(
+                            width: 900,
+                            height: 400,
+                            child: Card(
+                              child: Expanded(
+                                child: charts.LineChart(
+                                  _seriesLineData,
+                                  animate: true,
+                                  defaultRenderer:
+                                      new charts.LineRendererConfig(
+                                          includePoints: true),
+                                  //POINTS
+                                  animationDuration: Duration(
+                                      seconds: 5), //DURATION OF ANIMATION
+                                  behaviors: [
+                                    new charts.SeriesLegend(
+                                      position: charts.BehaviorPosition.end,
+                                      outsideJustification: charts
+                                          .OutsideJustification.endDrawArea,
+                                      horizontalFirst: false,
+                                      desiredMaxRows: 2,
+                                      cellPadding: new EdgeInsets.only(
+                                          right: 4.0, bottom: 4.0),
+                                      entryTextStyle: charts.TextStyleSpec(
+                                          color: charts.Color(
+                                              r: 127, g: 63, b: 191),
+                                          fontFamily: 'Georgia',
+                                          fontSize: 11),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          'att BY MONTH',
+                          style: TextStyle(
+                              fontSize: 24.0, fontFamily: 'RobotoMono'),
+                        ),
+                        SizedBox(
+                            width: 900,
+                            height: 400,
+                            child: Card(
+                              child: Expanded(
+                                child: charts.LineChart(
+                                  _seriesLineData,
+                                  animate: true,
+                                  defaultRenderer:
+                                      new charts.LineRendererConfig(
+                                          includePoints: true),
+                                  //POINTS
+                                  animationDuration: Duration(
+                                      seconds: 5), //DURATION OF ANIMATION
+                                  behaviors: [
+                                    new charts.SeriesLegend(
+                                      position: charts.BehaviorPosition.end,
+                                      outsideJustification: charts
+                                          .OutsideJustification.endDrawArea,
+                                      horizontalFirst: false,
+                                      desiredMaxRows: 2,
+                                      cellPadding: new EdgeInsets.only(
+                                          right: 4.0, bottom: 4.0),
+                                      entryTextStyle: charts.TextStyleSpec(
+                                          color: charts.Color(
+                                              r: 127, g: 63, b: 191),
+                                          fontFamily: 'Georgia',
+                                          fontSize: 11),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
